22cc9da494496188fcde8c5bee4420d50818c369
Paul Olteanu <p.a.olteanu@gmail.com>
2021-12-01 14:06:56 -0500

    Format

--- src/bin/day_1_1.rs
+++ src/bin/day_1_1.rs
@@ -1,24 +1,15 @@
 use std::fs;
 
 fn main() {
-    let input =
-        fs::read_to_string("inputs/day_1.1.txt")
-        .unwrap();
+    let input = fs::read_to_string("inputs/day_1.1.txt").unwrap();
 
-    let measurements = input.lines();
-
-    let result=
-        measurements
+    let result = input
+        .lines()
         .map(|s| s.trim().parse::<u32>().unwrap())
         .collect::<Vec<_>>()
         .windows(2)
-        .filter(|x| {
-            if let [a, b] = x {
-                b > a
-            } else {
-                false
-            }
-        })
+        .flat_map(<[u32; 2]>::try_from)
+        .filter(|[a, b]| b > a)
         .count();
 
     println!("{:?}", result);
--- src/bin/day_1_2.rs
+++ src/bin/day_1_2.rs
@@ -1,33 +1,19 @@
 use std::fs;
 
 fn main() {
-    let input =
-        fs::read_to_string("inputs/day_1.1.txt")
-        .unwrap();
+    let input = fs::read_to_string("inputs/day_1.1.txt").unwrap();
 
-    let measurements = input.lines();
-
-    let result=
-        measurements
+    let result = input
+        .lines()
         .map(|s| s.trim().parse::<u32>().unwrap())
         .collect::<Vec<_>>()
         .windows(3)
-        .map(|x| {
-            if let [a, b, c] = x {
-                a + b + c
-            } else {
-                0
-            }
-        })
+        .flat_map(<[u32; 3]>::try_from)
+        .map(|[a, b, c]| a + b + c)
         .collect::<Vec<_>>()
         .windows(2)
-        .filter(|x| {
-            if let [a, b] = x {
-                b > a
-            } else {
-                false
-            }
-        })
+        .flat_map(<[u32; 2]>::try_from)
+        .filter(|[a, b]| b > a)
         .count();
 
     println!("{:?}", result);
